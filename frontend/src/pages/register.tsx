a4faf680cc72d097b2849f92f4bfe43b5fc99f00
Bryan Yi <bryanyi1206@gmail.com>
2021-07-29 16:01:37 -0700

    workflow for using custom hooks for graphql

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,25 +1,37 @@
 import React from "react";
 import { Formik, Form } from "formik";
-import {
-  FormControl,
-  FormLabel,
-  Input,
-  FormErrorMessage,
-  Box,
-  Button,
-} from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
+import { useMutation } from "urql";
 
 interface registerProps {}
 
+const REGISTER_MUT = `
+mutation Register($username: String!, $password: String!){
+  register(options: { username: $username, password: $password }) {
+		errors{
+      field
+      message
+    }
+    user{
+      id
+      username
+    }
+  }
+}
+`;
+
 const Register: React.FC<registerProps> = ({}) => {
+  const [, register] = useMutation(REGISTER_MUT);
+
   return (
     <Wrapper variant="small">
       <Formik
         initialValues={{ username: "", password: "" }}
-        onSubmit={(values) => {
+        onSubmit={async (values) => {
           console.log(values);
+          const response = await register(values);
         }}
       >
         {({ isSubmitting }) => (
@@ -39,9 +51,10 @@ const Register: React.FC<registerProps> = ({}) => {
             </Box>
             <Button
               mt={4}
-              isLoading={isSubmitting}
               type="submit"
-              variantColor="teal"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+              variant="outline"
             >
               register
             </Button>
