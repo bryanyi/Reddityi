29441715e1c2dd9522804cb4cf5a254812826a64
Bryan Yi <bryanyi1206@gmail.com>
2021-08-10 16:33:48 -0700

    working on logout

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,10 +1,11 @@
-import { Box, Flex, Link } from "@chakra-ui/react";
+import { Box, Flex, Link, Button } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
-import { useMeQuery } from "../generated/graphql";
+import { useLoginMutation, useMeQuery } from "../generated/graphql";
 interface NavBarProps {}
 
 export const NavBar: React.FC<NavBarProps> = ({}) => {
+  const [{ fetching: logoutFetching }, logout] = useLoginMutation();
   const [{ data, fetching }] = useMeQuery();
   let body = null;
 
@@ -27,11 +28,24 @@ export const NavBar: React.FC<NavBarProps> = ({}) => {
   }
   // user is logged in
   else {
-    body = <Box>{data.me.username}</Box>;
+    body = (
+      <Flex>
+        <Box mr={3}>{data.me.username}</Box>
+        <Button
+          onClick={() => {
+            logout();
+          }}
+          isLoading={logoutFetching}
+          variant="link"
+        >
+          logout
+        </Button>
+      </Flex>
+    );
   }
 
   return (
-    <Flex bg="tomato" p={4}>
+    <Flex bg="tan" p={4}>
       <Box ml={"auto"}>{body}</Box>
     </Flex>
   );
