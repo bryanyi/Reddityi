2abe7a4ca30b5aab3076fd3cdacb82aef68dfccf
Bryan Yi <bryanyi1206@gmail.com>
2021-08-19 11:49:51 -0700

    completed forgot password

--- frontend/src/pages/forgot-password.tsx
+++ frontend/src/pages/forgot-password.tsx
@@ -1,49 +1,49 @@
 import { Box, Flex, Link, Button } from "@chakra-ui/react";
 import { Formik, Form } from "formik";
 import { withUrqlClient } from "next-urql";
-import router from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import { InputField } from "../components/InputField";
 import { Wrapper } from "../components/Wrapper";
+import { useForgotPasswordMutation } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
-import { toErrorMap } from "../utils/toErrorMap";
-import login from "./login";
 
-interface forgotPasswordProps {}
+interface ForgotPasswordProps {}
 
-const forgotPassword: React.FC<forgotPasswordProps> = ({}) => {
+const ForgotPassword: React.FC<ForgotPasswordProps> = ({}) => {
+  const [complete, setComplete] = useState(false);
+  const [, forgotPassword] = useForgotPasswordMutation();
+
   return (
     <Wrapper variant="small">
       <Formik
-        initialValues={{ usernameOrEmail: "", password: "" }}
-        onSubmit={async (values, { setErrors }) => {
-          const response = await login(values);
-          if (response.data?.login.errors) {
-            setErrors(toErrorMap(response.data.login.errors));
-          } else if (response.data?.login.user) {
-            // worked
-            router.push("/");
-          }
+        initialValues={{ email: "" }}
+        onSubmit={async (values) => {
+          await forgotPassword(values);
+          setComplete(true);
         }}
       >
-        {({ isSubmitting }) => (
-          <Form>
-            <InputField name="email" placeholder="email" label="email" />
+        {({ isSubmitting }) =>
+          complete ? (
+            <Box>If email exists, email has been sent</Box>
+          ) : (
+            <Form>
+              <InputField name="email" placeholder="email" label="email" />
 
-            <Button
-              mt={4}
-              type="submit"
-              isLoading={isSubmitting}
-              colorScheme="teal"
-              variant="outline"
-            >
-              forgot password
-            </Button>
-          </Form>
-        )}
+              <Button
+                mt={4}
+                type="submit"
+                isLoading={isSubmitting}
+                colorScheme="teal"
+                variant="outline"
+              >
+                forgot password
+              </Button>
+            </Form>
+          )
+        }
       </Formik>
     </Wrapper>
   );
 };
 
-export default withUrqlClient(createUrqlClient)(forgotPassword);
+export default withUrqlClient(createUrqlClient)(ForgotPassword);
